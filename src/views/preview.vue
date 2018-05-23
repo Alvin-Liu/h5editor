308a6e822dc6f38065f226336d00786b5ff8f7ff
Alvin-Liu <642451695@qq.com>
2018-05-23 22:22:29 +0800

    预览页面添加返回并处理修改相邻样式位置

--- src/views/preview.vue
+++ src/views/preview.vue
@@ -1,34 +1,39 @@
 <template>
-  <div id="preview" class="m-simulator">
-    <div class="wrap">
-      <div class="bgm-btn" 
-        v-if="h5Config.bgm"
-        :class="{rotate: isPlay}" 
-        @click="togglePlay">
-        <audio ref="music" :src="h5Config.bgm" autoplay preload="auto" loop></audio>
-      </div>
-      <swiper :options="swiperOption" ref="mySwiper">
-        <swiper-slide 
-          v-for="(page, idx) in pages"
-          :key="page.id">
-          <div class="wrap-bg" 
-            :style="{
-              backgroundColor: page.css.bgc,
-              backgroundImage: 'url(' + page.css.bgi + ')'
-            }"></div>
-          <div
-            v-for="comp in page.comps"
-            class="animated"
-            :class="{[animations[comp.anim.type]['class']]: activePage === idx}"
-            :key="comp.id">
-            <comp-lists
-              :comp="comp"
-              :style="comp.css | calcStyle"
-              class="comp"
-            />
+  <div class="preview">
+    <router-link to="/" class="u-back"><i class="el-icon-arrow-left"></i>返回</router-link>
+    <div class="m-mobile">
+      <div class="m-simulator">
+        <div class="wrap">
+          <div class="bgm-btn"
+            v-if="h5Config.bgm"
+            :class="{rotate: isPlay}" 
+            @click="togglePlay">
+            <audio ref="music" :src="h5Config.bgm" autoplay preload="auto" loop></audio>
           </div>
-        </swiper-slide>
-      </swiper>
+          <swiper :options="swiperOption" ref="mySwiper">
+            <swiper-slide 
+              v-for="(page, idx) in pages"
+              :key="page.id">
+              <div class="wrap-bg" 
+                :style="{
+                  backgroundColor: page.css.bgc,
+                  backgroundImage: 'url(' + page.css.bgi + ')'
+                }"></div>
+              <div
+                v-for="comp in page.comps"
+                class="animated"
+                :class="{[animations[comp.anim.type]['class']]: activePage === idx}"
+                :key="comp.id">
+                <comp-lists
+                  :comp="comp"
+                  :style="comp.css | calcStyle"
+                  class="comp"
+                />
+              </div>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -113,3 +118,58 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  $view_width: 320px;
+  $view_height: 486px;
+
+  .preview {
+    overflow: hidden;
+  }
+  .m-mobile {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    .comp {
+      position: absolute !important;
+    }
+  }
+  
+  .m-simulator {
+    position: relative;
+    width: $view_width + 8px;
+    height: $view_height + 74px;
+    background-image: url(../assets/phonewhite-7a04c3.svg);
+    background-repeat: no-repeat;
+    box-shadow: 0 2px 8px rgba(0,0,0,.15);
+    border-radius: 40px;
+    > .wrap {
+      position: absolute;
+      top: 37px;
+      left: 4px;
+      width: $view_width;
+      height: $view_height;
+      background-color: #fff
+    }
+  }
+
+  .wrap-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-origin: content-box;
+    background-position: 50% 50%;
+  }
+
+  .u-back {
+    float: left;
+    margin: 10px;
+    color: #409eff;
+    font-size: 18px;
+    font-weight: bold;
+  }  
+</style>
