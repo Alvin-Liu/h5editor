a8d0a147e3d1560074416834c1a11976e08c9f53
Alvin-Liu <642451695@qq.com>
2018-06-03 18:51:42 +0800

    漏提交文件

---
+++ src/modules/ComponentRenderer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :compid="comp.id"
+    class="comp"
+    :style="comp.css | calcStyle('rotate')"
+    :is="BASE_COMP_NAME + comp.name">
+  </div>
+</template>
+
+<script>
+import BaseComps from '@/components/index.js'
+
+const BASE_COMP_NAME = 'Base'
+
+export default {
+  name: 'ComponentRenderer',
+  props: {
+    comp: {
+      type: Object,
+      required: true
+    }
+  },
+  created () {
+    this.BASE_COMP_NAME = BASE_COMP_NAME
+  },
+  components: BaseComps
+}
+</script>
